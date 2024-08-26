257f2bdbab73fd34b00c27b78f562f9364c9ee0c
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-08-26 07:46:50 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -5,3 +5,8 @@ def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   
   return variable_name
+
+user_input=input("Enter a number: ")
+
+if(user_input=="1"):
+  print(hello_world())
